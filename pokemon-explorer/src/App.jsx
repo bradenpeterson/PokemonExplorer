58dcd80b3e895966a8692c151db483f5da3adce6
Braden Peterson <peterson.braden2@gmail.com>
2025-12-06 16:56:13 -0700

    Begun App component, created temporary expports for other components

--- pokemon-explorer/src/App.jsx
+++ pokemon-explorer/src/App.jsx
@@ -1,9 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { SearchBar } from './components/SearchBar.jsx'
+import { LoadingSpinner } from './components/LoadingSpinner.jsx'
+import { Card } from './components/Card.jsx'
+import { ErrorMessage } from './components/ErrorMessage.jsx'
+import { fetchPokemonList, fetchPokemonDetails, fetchBatchDetails } from './services/pokemonApi.js'
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([])
+  const [filteredPokemonList, setFilteredPokemonList] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [filterType, setFilterType] = useState('all')
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetchPokemonList()
+      .then(async (list) => {
+        const detailedList = await fetchBatchDetails(list)
+        setPokemonList(detailedList)
+        setFilteredPokemonList(detailedList)
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        setError('Failed to fetch Pokémon data.')
+        setIsLoading(false)
+      })
+  }, [])
 
   return (
     <>
+      <h1>Pokémon Explorer</h1>
     </>
   )
 }
---
+++ pokemon-explorer/src/components/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+export function ErrorMessage() {
+    return (
+        <></>
+    );
+}
